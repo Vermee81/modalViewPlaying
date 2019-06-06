d93bbe2720ba809bc023e3c5c4b335d7854c063a
Vermee81 <hrksb5029@gmail.com>
2019-06-07 02:15:56 +0900

    [add] add second modal screen

---
+++ modalViewPlaying/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  modalViewPlaying
+//
+//  Created by Hiroo Kusaba on 2019/06/07.
+//  Copyright © 2019 Hiroo Kusaba. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
