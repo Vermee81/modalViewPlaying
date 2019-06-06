8a3a83af7944cee0243437b3485494b8d301d1d0
Vermee81 <hrksb5029@gmail.com>
2019-06-07 02:55:41 +0900

    [add] delegate patternでSecond Modal screenを閉じる

--- modalViewPlaying/FirstViewController.swift
+++ modalViewPlaying/FirstViewController.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, SecondViewControllerDelegate {
+    
+    
+    func secondViewController(_ viewController: SecondViewController, didTapButton button: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func firstButtonTapped(_ sender: UIButton) {
+        guard case let secondViewController as SecondViewController = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") else{
+            return
+        }
+        secondViewController.delegate = self
+        present(secondViewController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- modalViewPlaying/SecondViewController.swift
+++ modalViewPlaying/SecondViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    weak var delegate: SecondViewControllerDelegate?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        delegate?.secondViewController(self, didTapButton: sender)
+    }
+    
+}
+
+protocol SecondViewControllerDelegate: class{
+    func secondViewController(_ viewController: SecondViewController, didTapButton button: UIButton)
 }
