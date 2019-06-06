94e6e0bee6e70efd00463ac302b0c545a282b5aa
Vermee81 <hrksb5029@gmail.com>
2019-06-07 02:04:56 +0900

    Initial Commit

---
+++ modalViewPlaying/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  modalViewPlaying
+//
+//  Created by Hiroo Kusaba on 2019/06/07.
+//  Copyright © 2019 Hiroo Kusaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
